a3d16c2dbeb3218ad5c8f2cda5dbc9e78b96afcc
Savannah Younts <savyounts@gmail.com>
2018-09-19 18:48:26 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,12 +2,4 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-
-  def new
-  end
-
-  def create
-    Student.create(params[:student])
-    redirect_to students_path
-  end
 end
