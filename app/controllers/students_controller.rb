9bcc415d40f4ce176507413f3d0a02e84079f139
Savannah Younts <savyounts@gmail.com>
2018-09-19 18:48:31 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(params[:student])
+    redirect_to student_path
   end
 
 end
